66b5b164538ea3d915e7cc8828f5505771d3f9b3
ray708 <rayiqui@hotmail.com>
2021-03-18 00:00:33 -0500

    reto3

---
+++ js/teclado.js
@@ -0,0 +1,608 @@
+//casilla
+let casilla = document.getElementById("casilla");
+
+
+
+//let btnEsc = document.getElementById("btneEsc");
+let btnPal = document.getElementById("btnPal");
+let btnNum1 = document.getElementById("btnNum1");
+let btnNum2 = document.getElementById("btnNum2");
+let btnNum3 = document.getElementById("btnNum3");
+let btnNum4 = document.getElementById("btnNum4");
+let btnNum5 = document.getElementById("btnNum5");
+let btnNum6 = document.getElementById("btnNum6");
+let btnNum7 = document.getElementById("btnNum7");
+let btnNum8 = document.getElementById("btnNum8");
+let btnNum9 = document.getElementById("btnNum9");
+let btnNum0 = document.getElementById("btnNum0");
+let btnComilla = document.getElementById("btnComilla");
+let btnPregunta = document.getElementById("btnPregunta");
+let btnBorrar = document.getElementById("btnBorrar");
+
+
+//SEGUNDA LINEA
+let btnTab = document.getElementById("btnTab");
+let btnLetrq = document.getElementById("btnLetrq");
+let btnLetrw = document.getElementById("btnLetrw");
+let btnLetre = document.getElementById("btnLetre");
+let btnLetrr = document.getElementById("btnLetrr");
+let btnLetrt = document.getElementById("btnLetrt");
+let btnLetry = document.getElementById("btnLetry");
+let btnLetru = document.getElementById("btnLetru");
+let btnLetri = document.getElementById("btnLetri");
+let btnLetro = document.getElementById("btnLetro");
+let btnLetrp = document.getElementById("btnLetrp");
+let btnComilla2 = document.getElementById("btnComilla2");
+
+
+//TERCERA LINEA
+let btnBmay = document.getElementById("btnBmay");
+let btnLetra = document.getElementById("btnLetra");
+let btnLetrs = document.getElementById("btnLetrs");
+let btnLetrd = document.getElementById("btnLetrd");
+let btnLetrf = document.getElementById("btnLetrf");
+let btnLetrg = document.getElementById("btnLetrg");
+let btnLetrh = document.getElementById("btnLetrh");
+let btnLetrj = document.getElementById("btnLetrj");
+let btnLetrk = document.getElementById("btnLetrk");
+let btnLetrl = document.getElementById("btnLetrl");
+let btnLetrñ = document.getElementById("btnLetrñ");
+let btnComilla3 = document.getElementById("btnComilla3");
+let btnCorchete = document.getElementById("btnCorchete");
+
+//CUARTA LINEA
+let btnMayus1 = document.getElementById("btnMayus1");
+let btnFlechita = document.getElementById("btnFlechita");
+let btnLetrz = document.getElementById("btnLetrz");
+let btnLetrx = document.getElementById("btnLetrx");
+let btnLetrc = document.getElementById("btnLetrc");
+let btnLetrv = document.getElementById("btnLetrv");
+let btnLetrb = document.getElementById("btnLetrb");
+let btnLetrn = document.getElementById("btnLetrn");
+let btnLetrm = document.getElementById("btnLetrm");
+let btnComa = document.getElementById("btnComa");
+let btnPunto = document.getElementById("btnPunto");
+let btnGuion = document.getElementById("btnGuion");
+let btnArriba = document.getElementById("btnArriba");
+let btnMayus = document.getElementById("btnMayus");
+let btnSuprimir = document.getElementById("btnSuprimir");
+
+//QUINTA LINEA
+
+let btnFn = document.getElementById("btnFn");
+let btnCtrl = document.getElementById("btnCtrl");
+let btnWindow = document.getElementById("btnWindow");
+let btnLetralt = document.getElementById("btnLetralt");
+let btnLetrav = document.getElementById("btnLetrav");
+let btnLetrab = document.getElementById("btnLetrab");
+let btnLetran = document.getElementById("btnLetran");
+let btnLetram = document.getElementById("btnLetram");
+let btnFd = document.getElementById("btnFd");
+let btnPrint = document.getElementById("btnPrint");
+
+
+
+
+
+//BOTONES ESPECIALES
+let btnEspacio = document.getElementById("btnEspacio");
+let btnEnter = document.getElementById("btnEnter");
+
+
+//VARIABLES TEMPORALES PRIMERA LINEA
+
+let tecla = "";
+
+//FUNCIONES PRIMERA LINEA TECLADO
+
+const btnPalF = () =>{
+
+    casilla.innerHTML = tecla + "|";
+    tecla = tecla + "|";     
+}
+
+const btnNum1F = () =>{
+
+    casilla.innerHTML = tecla + "1";
+    tecla = tecla + "1";     
+}
+
+const btnNum2F = () =>{
+
+    casilla.innerHTML = tecla + "2";
+    tecla =  tecla + "2";     
+}
+
+const btnNum3F = () =>{
+
+    casilla.innerHTML = tecla + "3";
+    tecla = tecla + "3";     
+}
+
+const btnNum4F = () =>{
+
+    casilla.innerHTML = tecla + "4";
+    tecla = tecla + "4";     
+}
+
+const btnNum5F = () =>{
+
+    casilla.innerHTML = tecla + "5";
+    tecla = tecla + "5";     
+}
+
+const btnNum6F = () =>{
+
+    casilla.innerHTML = tecla + "6";
+    tecla = tecla + "6";     
+}
+
+const btnNum7F = () =>{
+
+    casilla.innerHTML = tecla + "7";
+    tecla = tecla + "7";     
+}
+
+const btnNum8F = () =>{
+
+    casilla.innerHTML = tecla + "8";
+    tecla = tecla + "8";     
+}
+
+const btnNum9F = () =>{
+
+    casilla.innerHTML = tecla + "9";
+    tecla = tecla + "9";     
+}
+
+const btnNum0F = () =>{
+
+    casilla.innerHTML = tecla + "0";
+    tecla = tecla + "0";     
+}
+
+
+const btnComillaF = () =>{
+
+    casilla.innerHTML = tecla + "'";
+    tecla = tecla + "'";     
+}
+
+const btnPreguntaF = () =>{
+
+    casilla.innerHTML = tecla + "¿";
+    tecla = tecla + "¿";     
+}
+
+//FUNCIONES FUNCIONES SEGUNDA LINEA TECLADO
+
+const btnTabF = () =>{
+
+    casilla.innerHTML = tecla + "tab";
+    tecla = tecla + "tab";     
+}
+
+const btnLetrqF = () =>{
+
+    casilla.innerHTML = tecla + "q";
+    tecla = tecla + "q";     
+}
+
+const btnLetrwF = () =>{
+
+    casilla.innerHTML = tecla + "w";
+    tecla = tecla + "w";     
+}
+
+const btnLetreF = () =>{
+
+    casilla.innerHTML = tecla + "e";
+    tecla = tecla + "e";     
+}
+
+const btnLetrrF = () =>{
+
+    casilla.innerHTML = tecla + "r";
+    tecla = tecla + "r";     
+}
+const btnLetrtF = () =>{
+
+    casilla.innerHTML = tecla + "t";
+    tecla = tecla + "t";     
+}
+const btnLetryF = () =>{
+
+    casilla.innerHTML = tecla + "y";
+    tecla = tecla + "y";     
+}
+const btnLetruF = () =>{
+
+    casilla.innerHTML = tecla + "u";
+    tecla = tecla + "u";     
+}
+const btnLetriF = () =>{
+
+    casilla.innerHTML = tecla + "i";
+    tecla = tecla + "i";     
+}
+const btnLetroF = () =>{
+
+    casilla.innerHTML = tecla + "o";
+    tecla = tecla + "o";     
+}
+const btnLetrpF = () =>{
+
+    casilla.innerHTML = tecla + "p";
+    tecla = tecla + "p";     
+}
+const btnComilla2F = () =>{
+
+    casilla.innerHTML = tecla + "`";
+    tecla = tecla + "`";     
+}
+
+//FUNCIONES TERCERA LINEA TECLADO
+
+const btnBmayF = () =>{
+
+    casilla.innerHTML = tecla + "Bmay";
+    tecla = tecla + "Bmay";     
+}
+
+const btnLetraF = () =>{
+
+    casilla.innerHTML = tecla + "a";
+    tecla = tecla + "a";     
+}
+const btnLetrsF = () =>{
+
+    casilla.innerHTML = tecla + "s";
+    tecla = tecla + "s";     
+}
+const btnLetrdF = () =>{
+
+    casilla.innerHTML = tecla + "d";
+    tecla = tecla + "d";     
+}
+const btnLetrfF = () =>{
+
+    casilla.innerHTML = tecla + "f";
+    tecla = tecla + "f";     
+}
+const btnLetrgF = () =>{
+
+    casilla.innerHTML = tecla + "g";
+    tecla = tecla + "g";     
+}
+const btnLetrhF = () =>{
+
+    casilla.innerHTML = tecla + "h";
+    tecla = tecla + "h";     
+}
+const btnLetrjF = () =>{
+
+    casilla.innerHTML = tecla + "j";
+    tecla = tecla + "j";     
+}
+const btnLetrkF = () =>{
+
+    casilla.innerHTML = tecla + "k";
+    tecla = tecla + "k";     
+}
+const btnLetrlF = () =>{
+
+    casilla.innerHTML = tecla + "l";
+    tecla = tecla + "l";     
+}
+const btnLetrñF = () =>{
+
+    casilla.innerHTML = tecla + "ñ";
+    tecla = tecla + "ñ";     
+}
+const btnComilla3F = () =>{
+
+    casilla.innerHTML = tecla + "´";
+    tecla = tecla + "´";     
+}
+const btnCorcheteF = () =>{
+
+    casilla.innerHTML = tecla + "}";
+    tecla = tecla + "}";     
+}
+
+//FUNCIONES CUARTA LINEA TECLADO
+
+const btnMayus1F = () =>{
+
+  casilla.innerHTML = tecla + "Mayus";
+  tecla = tecla + "Mayus";     
+}
+
+const btnFlechitaF = () =>{
+
+  casilla.innerHTML = tecla + ">";
+  tecla = tecla + ">";     
+}
+const btnLetrzF = () =>{
+
+  casilla.innerHTML = tecla + "z";
+  tecla = tecla + "z";     
+}
+const btnLetrxF = () =>{
+
+  casilla.innerHTML = tecla + "x";
+  tecla = tecla + "x";     
+}
+const btnLetrcF = () =>{
+
+  casilla.innerHTML = tecla + "c";
+  tecla = tecla + "c";     
+}
+const btnLetrvF = () =>{
+
+  casilla.innerHTML = tecla + "v";
+  tecla = tecla + "v";     
+}
+const btnLetrbF = () =>{
+
+  casilla.innerHTML = tecla + "b";
+  tecla = tecla + "b";     
+}
+const btnLetrnF = () =>{
+
+  casilla.innerHTML = tecla + "n";
+  tecla = tecla + "n";     
+}
+const btnLetrmF = () =>{
+
+  casilla.innerHTML = tecla + "m";
+  tecla = tecla + "m";     
+}
+const btnComaF = () =>{
+
+  casilla.innerHTML = tecla + ",";
+  tecla = tecla + ",";     
+}
+const btnPuntoF = () =>{
+
+  casilla.innerHTML = tecla + ".";
+  tecla = tecla + ".";     
+}
+const btnGuionF = () =>{
+
+  casilla.innerHTML = tecla + "-";
+  tecla = tecla + "-";     
+}
+const btnArribaF = () =>{
+
+  casilla.innerHTML = tecla + "UP";
+  tecla = tecla + "UP";     
+}
+const btnMayus2F = () =>{
+
+  casilla.innerHTML = tecla + "Mayus";
+  tecla = tecla + "Mayus";     
+}
+
+const btnSuprimirF = () =>{
+
+  casilla.innerHTML = tecla + "Supr";
+  tecla = tecla + "Supr";     
+}
+
+//FUNCIONES CUARTA LINEA TECLADO
+
+//FUNCIONES BOTONES PRUEBA
+const btnEspacioF = () =>{
+
+    casilla.innerHTML = tecla + " ";
+    tecla = tecla + " ";
+
+}
+
+const btnEnterF = () =>{
+
+    casilla.innerHTML = tecla + "<br>";
+    tecla = tecla + "<br>";
+    
+}
+
+
+//EVENTOS PRIMERA LINEA
+btnPal.onclick = function () {
+    btnPalF();   
+  }
+
+btnNum1.onclick = function () {
+    btnNum1F();   
+  }
+
+btnNum2.onclick = function () {
+    btnNum2F();   
+  }
+
+btnNum3.onclick = function () {
+    btnNum3F();   
+  }
+
+btnNum4.onclick = function () {
+    btnNum4F();   
+  }
+
+btnNum5.onclick = function () {
+    btnNum5F();   
+  }
+
+btnNum6.onclick = function () {
+    btnNum6F();   
+  }
+
+btnNum7.onclick = function () {
+    btnNum7F();   
+  }
+
+btnNum8.onclick = function () {
+    btnNum8F();   
+  }
+
+btnNum9.onclick = function () {
+    btnNum9F();   
+  }
+
+btnNum0.onclick = function () {
+    btnNum0F();   
+  }
+
+btnComilla.onclick = function () {
+    btnComillaF();   
+  }
+
+btnPregunta.onclick = function () {
+    btnPreguntaF();   
+  }
+
+
+//EVENTOS SEGUNDA LINEA
+btnTab.onclick = function () {
+    btnTabF();   
+  }
+
+btnLetrq.onclick = function () {
+    btnLetrqF();   
+  }
+btnLetrw.onclick = function () {
+    btnLetrwF();   
+  }
+btnLetre.onclick = function () {
+    btnLetreF();   
+  }
+
+btnLetrr.onclick = function () {
+    btnLetrrF();   
+  }
+
+btnLetrt.onclick = function () {
+    btnLetrtF();   
+  }
+btnLetry.onclick = function () {
+    btnLetryF();   
+  }
+btnLetru.onclick = function () {
+    btnLetruF();   
+  }
+
+btnLetri.onclick = function () {
+    btnLetriF();   
+  }
+
+btnLetro.onclick = function () {
+    btnLetroF();   
+  } 
+
+btnLetrp.onclick = function () {
+    btnLetrpF();   
+  }
+
+btnComilla2.onclick = function () {
+    btnComilla2F();   
+  }
+
+
+//EVENTOS TERCERA LINEA
+btnBmay.onclick = function () {
+    btnBmayF();   
+  }
+btnLetra.onclick = function () {
+    btnLetraF();   
+  }
+btnLetrs.onclick = function () {
+    btnLetrsF();   
+  }
+btnLetrd.onclick = function () {
+    btnLetrdF();   
+  }
+btnLetrf.onclick = function () {
+    btnLetrfF();   
+  }
+  btnLetrg.onclick = function () {
+    btnLetrgF();   
+  }
+btnLetrh.onclick = function () {
+    btnLetrhF();   
+  }
+btnLetrj.onclick = function () {
+    btnLetrjF();   
+  }
+btnLetrk.onclick = function () {
+    btnLetrkF();   
+  }
+btnLetrl.onclick = function () {
+    btnLetrlF();   
+  }
+btnLetrñ.onclick = function () {
+    btnLetrñF();   
+  }
+btnComilla3.onclick = function () {
+    btnComilla3F();   
+  }
+btnCorchete.onclick = function () {
+    btnCorcheteF();   
+  }
+//EVENTOS CUARTA LINEA
+ 
+btnMayus1.onclick = function () {
+  btnMayus1F();  
+}
+
+//btnFlechita.onclick = function () {
+  //btnFlechitaF();  
+//}
+
+btnLetrz.onclick = function () {
+  btnLetrzF();   
+}
+btnLetrx.onclick = function () {
+  btnLetrxF();   
+}
+btnLetrc.onclick = function () {
+  btnLetrcF();   
+}
+btnLetrv.onclick = function () {
+  btnLetrvF();   
+}
+btnLetrb.onclick = function () {
+  btnLetrbF();   
+}
+btnLetrn.onclick = function () {
+  btnLetrnF();   
+}
+btnLetrm.onclick = function () {
+  btnLetrmF();   
+}
+btnComa.onclick = function () {
+  btnComaF();   
+}
+btnPunto.onclick = function () {
+  btnPuntoF();   
+}
+btnGuion.onclick = function () {
+  btnGuionF();   
+}
+btnArriba.onclick = function () {
+  btnArribaF();   
+}
+btnMayus2.onclick = function () {
+  btnMayus2F();   
+}
+btnSuprimir.onclick = function () {
+  btnSuprimirF();   
+}
+  
+//EVENTO ESPECIALES
+  btnEspacio.onclick = function () {
+    btnEspacioF();   
+  }
+
+  btnEnter.onclick = function () {
+    btnEnterF();   
+  }
